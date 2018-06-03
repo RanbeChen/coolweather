108a39210cb4f450ebe06a15f48812651c9775c2
RanbeChen <770576531@qq.com>
2018-06-03 17:22:45 +0800

    增加后台自动更新天气的功能

--- app/src/main/java/com/example/ranbe/coolweather/ui/ChooseAreaFragment.java
+++ app/src/main/java/com/example/ranbe/coolweather/ui/ChooseAreaFragment.java
@@ -16,6 +16,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.ranbe.coolweather.MainActivity;
 import com.example.ranbe.coolweather.R;
 import com.example.ranbe.coolweather.db.City;
 import com.example.ranbe.coolweather.db.County;
@@ -77,10 +78,17 @@ public class ChooseAreaFragment extends Fragment {
                     queryCounties();
                 } else if (currentLevel == LEVEL_COUNTY){
                     String weatherId = countyList.get(position).getWeatherId();
-                    Intent intent = new Intent(getActivity(),WeatherActivity.class);
-                    intent.putExtra("weather_id",weatherId);
-                    startActivity(intent);
-                    getActivity().finish();
+                    if (getActivity() instanceof MainActivity){
+                        Intent intent = new Intent(getActivity(),WeatherActivity.class);
+                        intent.putExtra("weather_id",weatherId);
+                        startActivity(intent);
+                        getActivity().finish();
+                    }else if (getActivity() instanceof WeatherActivity) {
+                        WeatherActivity activity = (WeatherActivity) getActivity();
+                        activity.drawerLayout.closeDrawers();
+                        activity.swipeRefresh.setRefreshing(true);
+                        activity.requestWeather(weatherId);
+                    }
                 }
             }
         });
